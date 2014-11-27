90d27ad86f0337d2e6f9280dd0d9959013bb81c1
Akira Sosa <akirasosa@gmail.com>
2014-11-27 19:32:29 +0700

    gulp-rimraf instead of clean

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
-var clean = require('gulp-clean');
+var rimraf = require('gulp-rimraf');
 
 gulp.task('clean', function () {
   return gulp.src('build', {read: false})
-    .pipe(clean());
+    .pipe(rimraf());
 });
