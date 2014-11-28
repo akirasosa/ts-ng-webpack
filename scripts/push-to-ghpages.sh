099eb78c15a0f6c142b1434aa21ec644f3a22d24
Akira Sosa <akirasosa@gmail.com>
2014-11-28 19:05:47 +0700

    fixed script to push gh-pages

--- scripts/push-to-ghpages.sh
+++ scripts/push-to-ghpages.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-rm -rf dist
+rm -rf out || exit 0;
+mkdir out;
 gulp build
-cp -rf dist dist.bk
-git checkout gh-pages
-rm -rf dist
-mv dist.bk dist
-git add dist
-hash=$(git rev-parse --short master)
-git commit -m "release ${hash}"
-git push
-git checkout master
+
+(
+cd out
+git init
+git config user.name "Travis-CI"
+git config user.email "travis@example.com"
+cp ../index.html .
+cp -rf ../dist .
+git add .
+git commit -m "Deployed to Github Pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+)
