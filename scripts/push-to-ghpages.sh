f0e7baf04c61123f74263ae2f8bf17748448ee93
Akira Sosa <akirasosa@gmail.com>
2014-11-28 22:25:55 +0700

    dev null

--- scripts/push-to-ghpages.sh
+++ scripts/push-to-ghpages.sh
@@ -14,5 +14,5 @@ cp ../index.html .
 cp -rf ../dist .
 git add .
 git commit -m "Deployed to Github Pages"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 )
